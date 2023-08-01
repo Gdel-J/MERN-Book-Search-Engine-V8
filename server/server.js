a2a3b68c7fa32fac594ff9932d6f3123b0726515
Gdel-J <jessygdelvecchio@gmail.com>
2023-08-01 14:18:35 -0400

    trying  to troubleshoot the npm run build...

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
 // add apollo server
-const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 // We don't need:   const routes = require('./routes'); for GraphQL API
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -20,14 +21,14 @@ app.use(express.urlencoded({ extended: false })); //If the URL-encoded format is
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 //app.use(routes);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -35,7 +36,7 @@ const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
 
-  db.once("open", () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
